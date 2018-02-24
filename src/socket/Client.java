953f00ce538e0c5e01e47407194e18b41c73c719
n0rfolk <n0rfolk.itsec@gmail.com>
2018-02-24 17:01:38 +0100

    Client and Server filled, Frame modified

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,44 +1,47 @@
 package socket;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-
 import utilities.Frame;
 import utilities.LogWriter;
 
 public class Client {
 	//Client attributes
 	Socket socket;
-	//InputStream in;
 	OutputStream out;
 	
 	// default Client constructor
 	public Client() throws UnknownHostException, IOException {
-		this.socket = new Socket("172.23.180.64", 9999);
-		//this.in = socket.getInputStream();
+		String defautIP = "172.23.180.64";
+		this.socket = new Socket(defautIP, 9999);
 		this.out = socket.getOutputStream();
+		log("created new client instance with default ip " + defautIP);
 	}
 	
 	//Client constructor with given IP address
 	public Client(String ipAddress) throws UnknownHostException, IOException {
 		this.socket = new Socket(ipAddress, 9999);
-		//this.in = socket.getInputStream();
 		this.out = socket.getOutputStream();
+		log("created new client instance with ip " + ipAddress);
 	}
 	
 	//closing session
-	public void closeSession() throws IOException {
+	private void closeSession() throws IOException {
 		this.socket.close();
+		log("session closed");
 	}
 	
 	//sending frame
-	private void sendFrame(Frame f) throws IOException {
+	public void sendFrame(Frame f) throws IOException {
 		this.out.write(f.getBytesWithCRC());
+		log("frame sent");
 		closeSession();
 	}
 	
+	//prints logs
+	private void log(String s) {
+		LogWriter.log(this.getClass().getName(), s);
+	}
 }
--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,5 +1,51 @@
 package socket;
 
-public class Server {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import utilities.Frame;
+import utilities.LogWriter;
+
+public class Server {
+	//Server attributes
+	ServerSocket serverSocket;
+	Socket socket;
+	InputStream in;
+	// TODO: FpgaWriter
+
+	// default Server constructor
+	public Server() throws UnknownHostException, IOException {
+		this.serverSocket = new ServerSocket(9999);
+		this.socket = serverSocket.accept();
+		this.in = socket.getInputStream();
+		log("created new instance");
+	}
+	
+	//Server start
+	void run() throws IOException {
+		readFrame();
+		// TODO: FpgaWriter
+		closeSession();
+	}
+	
+	//Read frame
+	private void readFrame() throws IOException {
+		byte[] buffer = new byte[Frame.SIZE];
+		this.in.read(buffer);
+		log("frame read");
+	}
+	
+	//closing server session
+	private void closeSession() throws IOException {
+		this.socket.close();
+		this.serverSocket.close();
+		log("session closed");
+	}
+	//prints logs
+	private void log(String s) {
+		LogWriter.log(this.getClass().getName(), s);
+	}
 }
