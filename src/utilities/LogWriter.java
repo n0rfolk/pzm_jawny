a05d9fab498aea381b509782071439510ed51216
erndsaekal <erndsaekal@gmail.com>
2018-02-24 15:01:48 +0100

    Updated LogWriter

--- src/utilities/LogWriter.java
+++ src/utilities/LogWriter.java
@@ -4,7 +4,20 @@ public class LogWriter {
 	
 	
 	public static void log(String s) {
-		System.out.println("Engine: " + s);
+		System.out.println(KDebug.getCallerClassName() + ": " + s);
 	
+	}
+	
+	public static class KDebug {
+	    public static String getCallerClassName() { 
+	        StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
+	        for (int i=1; i<stElements.length; i++) {
+	            StackTraceElement ste = stElements[i];
+	            if (!ste.getClassName().equals(KDebug.class.getName()) && ste.getClassName().indexOf("java.lang.Thread")!=0) {
+	                return ste.getClassName();
+	            }
+	        }
+	        return null;
+	     }
 	}
 }
